9538be98cc66d65dd4b59b72110ee3d5c5ed2112
laurelfulford <laurelfulford@gmail.com>
2016-02-19 14:10:45 -0800

    Adding all SVGs for testing.

--- index.php
+++ index.php
@@ -20,6 +20,10 @@ get_header(); ?>
 			<?php echo file_get_contents( get_template_directory() . '/images/pencil.svg' ); ?>
 			<?php echo file_get_contents( get_template_directory() . '/images/pen.svg' ); ?>
 			<?php echo file_get_contents( get_template_directory() . '/images/movie-ticket.svg' ); ?>
+			<?php echo file_get_contents( get_template_directory() . '/images/pencil-shaving.svg' ); ?>
+			<?php echo file_get_contents( get_template_directory() . '/images/little-pencil.svg' ); ?>
+			<?php echo file_get_contents( get_template_directory() . '/images/stamp.svg' ); ?>
+			<?php echo file_get_contents( get_template_directory() . '/images/pencil-sharpener.svg' ); ?>
 
 		<?php
 		if ( have_posts() ) :
