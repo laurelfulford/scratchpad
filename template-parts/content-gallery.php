e7e1dcf3b770c73b3c82eb07301c0c153b4ea2c4
laurelfulford <laurelfulford@gmail.com>
2016-02-19 08:18:37 -0800

    Initial commit of files with some roughed in styles/markup.

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Template part for displaying gallery posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Scratchpad
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php
+		if ( get_post_gallery() ) { ?>
+			<div class="entry-gallery">
+				<?php echo get_post_gallery(); ?>
+			</div><!-- .entry-gallery -->
+		<?php
+		} // endif get_post_gallery()
+	?>
+
+	<header class="entry-header">
+		<?php if ( is_sticky() && is_home() ) { ?>
+			<span class="featured-post"><?php esc_html_e( 'Featured', 'scratchpad' ); ?></span>
+		<?php } ?>
+
+		<?php if ( ! is_single() ) {
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		} else {
+			the_title( '<h2 class="entry-title">', '</h2>' );
+		} ?>
+
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php scratchpad_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+	<?php if ( is_single() ) { ?>
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'scratchpad' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
+
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'scratchpad' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+	<?php } ?>
+
+	<footer class="entry-footer">
+		<?php  ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
---
+++ template-parts/content-image.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Scratchpad
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail() ) { ?>
+		<div class="featured-image">
+			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+			<div class="photo-corners">
+				<?php echo file_get_contents( get_template_directory() . '/images/photo-corners.svg' ); ?>
+				<?php echo file_get_contents( get_template_directory() . '/images/photo-corners.svg' ); ?>
+				<?php echo file_get_contents( get_template_directory() . '/images/photo-corners.svg' ); ?>
+				<?php echo file_get_contents( get_template_directory() . '/images/photo-corners.svg' ); ?>
+			</div><!-- .photo-corners -->
+		</div><!-- .featured-image -->
+	<?php } ?>
+
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php scratchpad_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+</article><!-- #post-## -->
