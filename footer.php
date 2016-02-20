71ecfd8ced5cef28cd5837881caa874f684a6591
laurelfulford <laurelfulford@gmail.com>
2016-02-19 18:09:44 -0800

    Adding sketchy line to footer.

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
 			<?php } ?>
 
 			<div class="site-info">
+				<?php echo file_get_contents( get_template_directory() . '/images/line.svg' ); ?>
 				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'scratchpad' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'scratchpad' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
 				<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'scratchpad' ), 'Scratchpad', '<a href="http://wordpress.com/themes/scratchpad/" rel="designer">Automattic</a>' ); ?>
