b5450eef34414301641f7eaae630b5e0b8b81195
laurelfulford <laurelfulford@gmail.com>
2016-02-23 10:55:42 -0800

    Switching out for custom post navigation.

--- single.php
+++ single.php
@@ -17,10 +17,7 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation(array(
-				'prev_text' => sprintf( '<span class="post-navigation-header">%s</span> %s', esc_html__( 'Previous Post', 'scratchpad' ), '%title' ),
-				'next_text' => sprintf( '<span class="post-navigation-header">%s</span> %s', esc_html__( 'Next Post', 'scratchpad' ), '%title' ),
-			));
+			scratchpad_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
