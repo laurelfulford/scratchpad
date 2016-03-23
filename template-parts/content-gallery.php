ec4a185097dadc78aabcac90078d7a01bf6c9cbb
laurelfulford <laurelfulford@gmail.com>
2016-03-23 14:56:00 -0700

    Removing 'single' fallback, since the post format specific content parts aren't used on single posts.

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -38,23 +38,6 @@
 		} ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_single() ) { ?>
-		<div class="entry-content">
-			<?php
-				the_content( sprintf(
-					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'scratchpad' ), array( 'span' => array( 'class' => array() ) ) ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				) );
-
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'scratchpad' ),
-					'after'  => '</div>',
-				) );
-			?>
-		</div><!-- .entry-content -->
-	<?php } ?>
-
 	<footer class="entry-meta">
 		<?php scratchpad_post_format(); ?>
 		<?php scratchpad_posted_on(); ?>
